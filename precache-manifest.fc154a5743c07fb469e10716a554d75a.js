b96414b8b253559b0bce643e89d1a945c7272a08
Clare Jung <clare.jung@hotmail.com>
2020-05-27 19:03:26 +0900

    Updates

---
+++ precache-manifest.fc154a5743c07fb469e10716a554d75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5139ec3e2866e84047ef8cdc9c58729",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "78c0fe7203c43221651e",
+    "url": "/movie_app_2020/static/css/main.6b542ff7.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c0fe7203c43221651e",
+    "url": "/movie_app_2020/static/js/main.e7a04549.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
